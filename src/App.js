24ee652e4ff8c1c103084a584379e91317333b50
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-08-04 23:08:07 +0200

    Create react app.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dreamware Games
+      </header>
+    </div>
+  );
+}
+
+export default App;
