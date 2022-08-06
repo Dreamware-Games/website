530c97262361118843db5b95cf24ba7d34f7c11c
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-08-07 00:44:02 +0200

    Styling.

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 import { Container, Nav, Navbar } from 'react-bootstrap'
 
+import '../assets/css/menubar.css'
+
 
 export const MenuBar = () => {
     return (
-        <Navbar bg='dark' variant='dark'>
+        <Navbar className='cstm-bg' variant='dark'>
             <Container>
                 <Navbar.Brand href='/'>Dreamware Games</Navbar.Brand>
                 <Nav className='me-auto'>
-                    <Nav.Link href='/games'>Games</Nav.Link>
+                    <Nav.Link href='/games' >Games</Nav.Link>
                     <Nav.Link href='/about'>About</Nav.Link>
                 </Nav>
             </Container>
