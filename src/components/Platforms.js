9fc9c356ace1cb9a116a3ad34c5349068cd4ebef
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-09-23 22:04:40 +0200

    Fontawesome

--- src/components/Platforms.js
+++ src/components/Platforms.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSteam, faXbox, faPlaystation } from '@fortawesome/free-brands-svg-icons'
+
 
 export const Platforms = () => {
     return (
         <Container className='mt-2 wrapper center'>
-            ...
-            {/*<FontAwesomeIcon icon="fa-brands fa-xbox" />*/}
+            <Row>
+                <Col><FontAwesomeIcon size='3x' icon={faSteam} /></Col>
+                <Col><FontAwesomeIcon size='3x' icon={faXbox} /></Col>
+                <Col><FontAwesomeIcon size='3x' icon={faPlaystation} /></Col>
+            </Row>
         </Container>
     )
 }
