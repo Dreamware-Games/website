41d295252cf70082b6c8af0732e92b10a6e00fdb
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-09-23 22:39:53 +0200

    Styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap'
 
 export const NotFound = () => {
     return (
-        <Container className='mt-2 wrapper center'>
+        <Container className='mt-2 wrapper'>
             404
         </Container>
     )
