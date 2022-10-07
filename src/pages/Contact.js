47ab20da34f425b92957c366f1ac823070870a70
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-10-07 17:36:16 +0200

    Contact form. Add emailjs.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Row, Col, Button, Container, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('sending...')
+
+        /*emailjs.sendForm('service_pn2j8zp', 'template_93f31ew', form.current, 'POSvSMWxvYWAOR8V2')
+            .then((result) => {
+                console.log(result.text)
+            }, (error) => {
+                console.log(error.text)
+            })*/
+    }
+
     return (
         <Container className='mt-2 wrapper'>
             <Container className='mt-4 mb-3 center'>
@@ -16,26 +32,26 @@ export const Contact = () => {
                     We would love to help realize your game idea or answer any questions you might have.
                 </Col>
                 <Col md>
-                    <Form>
+                    <Form ref={form} onSubmit={sendEmail}>
                         <Form.Group className='mb-3' controlId='formBasicName'>
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type='text' placeholder='You name' />
+                            <Form.Control name='name' type='text' placeholder='Your name' />
                             <Form.Text className='text-muted'>
                                 We'll never share your name with anyone else.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group className='mb-3' controlId='formBasicEmail'>
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' />
+                            <Form.Control name='email' type='email' placeholder='Enter email' />
                             <Form.Text className='text-muted'>
                                 We'll never share your email with anyone else.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group className='mb-3' controlId='formBasicMessage'>
                             <Form.Label>Message</Form.Label>
-                            <Form.Control as='textarea' placeholder='Enter message' rows={3} />
+                            <Form.Control name='message' as='textarea' placeholder='Enter message' rows={3} />
                         </Form.Group>
-                        <Button variant='dark' disabled type='submit'>
+                        <Button variant='dark' type='submit'>
                             Submit
                         </Button>
                     </Form>
