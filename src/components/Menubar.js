a0635b6b04fbbc5e5bfcca730f06a75c61ee6f69
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2022-12-25 21:01:37 +0100

    Styling. Collapse responsive menu on click.

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,16 +7,19 @@ import { faGamepad, faPaperPlane, faCircleInfo } from '@fortawesome/free-solid-s
 import '../assets/css/menubar.css'
 
 export const MenuBar = () => {
+
+    const [expanded, setExpanded] = useState(false)
+
     return (
-        <Navbar className='cstm-navbar' expand="lg" variant='dark'>
+        <Navbar expanded={expanded} className='cstm-navbar' expand="lg" variant='dark'>
             <Container>
-                <Navbar.Brand as={Link} to='/' className='cstm-navbar-home'>Dreamware Games™</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link as={Link} to='/games'><FontAwesomeIcon icon={faGamepad} />&nbsp;Games</Nav.Link>
-                        <Nav.Link as={Link} to='/about'><FontAwesomeIcon icon={faCircleInfo} />&nbsp;About</Nav.Link>
-                        <Nav.Link as={Link} to='/contact'><FontAwesomeIcon icon={faPaperPlane} />&nbsp;Contact</Nav.Link>
+                <Navbar.Brand as={Link} to='/' onClick={() => setExpanded(false)} className='cstm-navbar-home'>Dreamware Games™</Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav className='mr-auto navlink'>
+                        <Nav.Link as={Link} to='/games' onClick={() => setExpanded(false)}><FontAwesomeIcon icon={faGamepad} />&nbsp;Games</Nav.Link>
+                        <Nav.Link as={Link} to='/about' onClick={() => setExpanded(false)}><FontAwesomeIcon icon={faCircleInfo} />&nbsp;About</Nav.Link>
+                        <Nav.Link as={Link} to='/contact' onClick={() => setExpanded(false)}><FontAwesomeIcon icon={faPaperPlane} />&nbsp;Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,34 +2,30 @@ import React from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-import { Separator } from '../components/common/Separator';
-
 
 export const Home = () => {
     return (
-        <Container className='mt-2 wrapper'>
-            <Container className='mt-4 mb-3 center'>
-                <h1>Games for everyone!</h1>
+        <div className='hero-container'>
+            <Container className='mt-5 wrapper'>
+                <Row className='intro'>
+                    <Col className='mt-2 mb-2' md>
+                        Indie game development with a simple ambition:
+                        <br /><span className="intro-header">Games for everyone!</span>
+                    </Col>
+                    <Col className='mt-2 mb-2' md>
+                        Check out our ongoing projects and get in touch if you have any questions
+                        or an idea for a game you would like to develop or publish.
+                        <Container className='mt-4'>
+                            <Button className='m-2' as={Link} to='/games' variant='dark'>
+                                Games
+                            </Button>
+                            <Button as={Link} to='/contact' variant='dark'>
+                                Contact
+                            </Button>
+                        </Container>
+                    </Col>
+                </Row>
             </Container>
-            <Separator />
-            <Row className='intro'>
-                <Col className='mt-2 mb-2' md>
-                    Indie game development with a simple ambition:
-                    <br /><span className="intro-header">Games for everyone!</span>
-                </Col>
-                <Col className='mt-2 mb-2' md>
-                    Check out our ongoing projects and get in touch if you have any questions
-                    or an idea for a game you would like to develop or publish.
-                    <Container className='mt-4'>
-                        <Button className='m-2' as={Link} to='/games' variant='dark'>
-                            Games
-                        </Button>
-                        <Button as={Link} to='/contact' variant='dark'>
-                            Contact
-                        </Button>
-                    </Container>
-                </Col>
-            </Row>
-        </Container>
+        </div>
     )
 }
